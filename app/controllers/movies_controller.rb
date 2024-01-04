4314a24689558f09866e15bee8afbe02935f3734
jptran0 <jazminetra00@gmail.com>
2024-01-04 13:35:44 -0600

    reusing the new form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def new
+    @the_movie = Movie.new
     render template: "movies/new"
   end
 
@@ -35,7 +36,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      render template: "movies/with_errors"
+      render template: "movies/new"
     end
   end
 
